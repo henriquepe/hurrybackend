21099c5ebb1da6fd3c0f8923f3592939af98e1f4
henriquepe <henrique.p.espindola@gmail.com>
2020-10-22 08:01:37 -0300

    configuring the database to deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,14 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+    type: 'postgres',
+    host: 'hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'hurrydb',
+    password: 'hurrydbpassword',
+    entities: ['./dist/models/*.js'],
+    migrations: ['./dist/database/migrations/*.js'],
+    cli: {
+        migrationsDir: './dist/database/migrations',
+    },
+});
