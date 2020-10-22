f6c0616a180230865c97b464fb61c95d7b79798a
henriquepe <henrique.p.espindola@gmail.com>
2020-10-22 07:44:36 -0300

    configuring packagejson to deploy

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: 'c9225429014b4d57e2191f8bf08144aa',
+        expiresIn: '1d',
+    },
+};
---
+++ dist/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var index_1 = __importDefault(require("./routes/index"));
+require("./database");
+require("reflect-metadata");
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(cors_1.default());
+app.use(index_1.default);
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("this server started on port " + port);
+});
