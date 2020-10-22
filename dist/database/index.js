4f56c67910aea1d91705dd9648f35874a857ef65
henriquepe <henrique.p.espindola@gmail.com>
2020-10-22 08:07:27 -0300

    configuring the database to deploy

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,4 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var typeorm_1 = require("typeorm");
-typeorm_1.createConnection();
+typeorm_1.createConnection({
+    type: 'postgres',
+    host: 'hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'hurrydb',
+    password: 'hurrydbpassword',
+    entities: ['./dist/models/*.js'],
+    migrations: ['./dist/database/migrations/*.js'],
+    cli: {
+        migrationsDir: './dist/database/migrations',
+    },
+});
