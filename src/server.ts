036d8ece8f45dd8c851e8626d9a3421520553aa1
henriquepe <henrique.p.espindola@gmail.com>
2020-10-22 07:40:50 -0300

    configuring to deploy in heroku

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 import './database';
 import 'reflect-metadata';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
-const port = 3333;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`this server started on port ${port}`);
