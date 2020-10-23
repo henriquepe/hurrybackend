c994d923a9be304f44a3de5ab47cb1322be24c8e
henriquepe <henrique.p.espindola@gmail.com>
2020-10-23 10:10:50 -0300

    server database config

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,4 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var typeorm_1 = require("typeorm");
-typeorm_1.createConnection();
+typeorm_1.createConnection({
+    name: 'hurrybankconnection',
+    type: 'postgres',
+    database: 'hurrydb',
+    host: 'hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'hurrydb',
+    password: 'hurrydbpassword',
+    entities: ['./dist/models/**/*.js'],
+    migrations: ['./dist/database/**/migrations/*.js'],
+    cli: {
+        migrationsDir: './src/database/migrations',
+        entitiesDir: './src/models',
+    },
+});
+// {
+//     "name": "hurrybankconnection",
+//     "type": "postgres",
+//     "database": "hurrydb",
+//     "host": "hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com",
+//     "port": 5432,
+//     "username": "hurrydb",
+//     "password": "hurrydbpassword",
+//     "entities": "./dist/models/**/*.js",
+//     "migrations": [
+//         "./dist/database/**/migrations/*.js"
+//     ],
+//     "cli": {
+//         "migrationsDir": "./src/database/migrations",
+//         "entitiesDir": "./src/models"
+//     }
+// }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,35 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+    name: 'hurrybankconnection',
+    type: 'postgres',
+    database: 'hurrydb',
+    host: 'hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'hurrydb',
+    password: 'hurrydbpassword',
+    entities: ['./dist/models/**/*.js'],
+    migrations: ['./dist/database/**/migrations/*.js'],
+    cli: {
+        migrationsDir: './src/database/migrations',
+        entitiesDir: './src/models',
+    },
+});
+
+// {
+//     "name": "hurrybankconnection",
+//     "type": "postgres",
+//     "database": "hurrydb",
+//     "host": "hurrydbinstance2.c1ekyxn2vbik.us-east-2.rds.amazonaws.com",
+//     "port": 5432,
+//     "username": "hurrydb",
+//     "password": "hurrydbpassword",
+//     "entities": "./dist/models/**/*.js",
+//     "migrations": [
+//         "./dist/database/**/migrations/*.js"
+//     ],
+//     "cli": {
+//         "migrationsDir": "./src/database/migrations",
+//         "entitiesDir": "./src/models"
+//     }
+// }
