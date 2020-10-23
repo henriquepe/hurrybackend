f6697e6f50ebb00800484d3dc251875861dac8c9
henriquepe <henrique.p.espindola@gmail.com>
2020-10-23 08:49:29 -0300

    x

---
+++ dist/config/mail.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: '66b53a140d0c27',
+        pass: 'a73c37e0957ecb',
+    },
+};
