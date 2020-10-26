9b35f3787db5d978622d4bc670e46a41a6a12484
henriquepe <henrique.p.espindola@gmail.com>
2020-10-26 02:15:27 -0300

    choice of provider email

--- dist/config/mail.js
+++ dist/config/mail.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: 'smtp.gmail.com',
+    port: 587,
     auth: {
-        user: '66b53a140d0c27',
-        pass: 'a73c37e0957ecb',
+        user: 'henrique.p.espindola@gmail.com',
+        pass: 'rosa1702',
     },
 };
