ea843e8342b073303a1c484022d8ae9745823c29
henriquepe <henrique.p.espindola@gmail.com>
2020-10-27 12:42:03 -0300

    x

--- dist/services/UpdateAvatarService.js
+++ dist/services/UpdateAvatarService.js
@@ -12,9 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable no-useless-constructor */
-const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
 const typedi_1 = require("typedi");
 const typeorm_1 = require("typeorm");
 const upload_1 = __importDefault(require("../config/upload"));
@@ -30,13 +27,15 @@ let UpdateAvatarService = class UpdateAvatarService {
             throw new Error('Only authenticated users can change avatar');
         }
         const { directory } = upload_1.default;
-        if (user.avatar) {
-            const userAvatarFilePath = path_1.default.join(directory, user.avatar);
-            const userAvatarFileExists = await fs_1.default.promises.stat(userAvatarFilePath);
-            if (userAvatarFileExists) {
-                await fs_1.default.promises.unlink(userAvatarFilePath);
-            }
-        }
+        // if (user.avatar) {
+        //     const userAvatarFilePath = path.join(directory, user.avatar);
+        //     const userAvatarFileExists = await fs.promises.stat(
+        //         userAvatarFilePath,
+        //     );
+        //     if (userAvatarFileExists) {
+        //         await fs.promises.unlink(userAvatarFilePath);
+        //     }
+        // }
         user.avatar = avatarFilename;
         await this.usersRepository.save(user);
         return user;
