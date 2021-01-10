184f5c8d6dc394d45201a6c38f76d94e4952012e
henriquepe <henrique.p.espindola@gmail.com>
2021-01-10 20:38:21 -0300

    aws provider change

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,12 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-    // "type": "postgres",
-    // "url": "postgres://tbcwrcxxioxpva:49d6d2c87395a25218e22bd4bea7678fe26408b0cd91f9d35204410e26da4e6a@ec2-23-23-242-234.compute-1.amazonaws.com:5432/d5fgb7sl44hsau",
     "type": "postgres",
-    "host": "hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com",
-    "username": "hurrydb",
-    "database": "postgres",
-    "password": "hurrydatabase",
-    "port": 5432,
+    "url": "postgres://hurrydb:hurrydatabase@hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com:5432/postgres",
+    // "type": "postgres",
+    // "host": "hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com",
+    // "username": "hurrydb",
+    // "password": "hurrydatabase",
+    // "port": 5432,
 
     "entities": [
         "./dist/models/**/*.js",
