d09499fc92776d8a21e197bec873863c7a60e3c8
henriquepe <henrique.p.espindola@gmail.com>
2021-01-10 21:04:26 -0300

    aws provider change

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,35 @@
 /* eslint-disable prettier/prettier */
+
+import User from './src/models/User.entity';
+import Appointment from './src/models/Appointment.entity';
+import Post from './src/models/Post.entity';
+import MusicStyle from './src/models/MusicStyle.entity';
+import Drink from './src/models/Drink.entity';
+import EventType from './src/models/EventType.entity';
+
+
 module.exports = {
-    // "type": "postgres",
-    // "url": "postgres://hurrydb:hurrydatabase@hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com:5432/postgres",
     "type": "postgres",
-    "host": "hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com",
-    "username": "hurrydb",
-    "password": "hurrydatabase",
-    "port": 5432,
+    "url": "postgres://hurrydb:hurrydatabase@hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com:5432/postgres",
+    // "type": "postgres",
+    // "host": "hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com",
+    // "username": "hurrydb",
+    // "password": "hurrydatabase",
+    // "port": 5432,
 
     "entities": [
-        "./dist/models/*.js",
-        "./src/models/**/*.ts"
+        // "dist/models/*.js",
+        // "./src/models/**/*.ts"
+        User,
+        Appointment,
+        Post,
+        MusicStyle,
+        Drink,
+        EventType
     ],
     "migrations": [
-        "./dist/database/**/migrations/*.js",
-        "./src/database/**/migrations/*.ts"
+        "dist/database/**/migrations/*.js",
+        // "./src/database/**/migrations/*.ts"
     ],
     "cli": {
         "migrationsDir": "./src/database/migrations",
