7c26e7da5a6eaa362d0c3bb03667afcd795db43e
henriquepe <henrique.p.espindola@gmail.com>
2021-01-10 22:55:49 -0300

    some changes

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     type: 'postgres',
     url:
-        'postgres://hurrydb:hurrydatabase@hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com:5432/hurrydb',
+        'postgres://tbcwrcxxioxpva:49d6d2c87395a25218e22bd4bea7678fe26408b0cd91f9d35204410e26da4e6a@ec2-23-23-242-234.compute-1.amazonaws.com:5432/d5fgb7sl44hsau',
     // type: 'postgres',
     // host: 'hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com',
 
@@ -14,7 +14,7 @@ module.exports = {
         // eslint-disable-next-line no-path-concat
         // eslint-disable-next-line no-useless-concat
 
-        'dist/models/**/*.entity.js',
+        './dist/models/*.js',
         // "./src/models/**/*.ts"
         // User,
         // Appointment,
@@ -32,7 +32,7 @@ module.exports = {
         entitiesDir: [
             // './dist/models',
             // eslint-disable-next-line no-useless-concat
-            'dist/models/',
+            './dist/models/',
         ],
         // "entitiesDir": "./src/models"
     },
