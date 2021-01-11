58bd095e816a56419ea8c47849ce64897238c3a3
henriquepe <henrique.p.espindola@gmail.com>
2021-01-10 22:28:36 -0300

    aws provider change

--- ormconfig.js
+++ ormconfig.js
@@ -1,27 +1,21 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-path-concat */
-/* eslint-disable prefer-template */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-
-
 module.exports = {
     // "type": "postgres",
     // "url":
     //     "postgres://hurrydb:hurrydatabase@hurrydb.cvx6sc53gqyv.us-east-2.rds.amazonaws.com:5432/hurrydb",
-    "type": "postgres",
-    "host": "hurrydb-instance-1-us-east-2b.cvx6sc53gqyv.us-east-2.rds.amazonaws.com",
+    type: 'postgres',
+    host:
+        'hurrydb-instance-1-us-east-2b.cvx6sc53gqyv.us-east-2.rds.amazonaws.com',
 
-    "username": "hurrydb",
-    "password": "hurrydatabase",
-    "port": 5432,
-    "database": "postgres",
+    username: 'hurrydb',
+    password: 'hurrydatabase',
+    port: 5432,
+    database: 'postgres',
 
-
-    "entities": [
+    entities: [
         // eslint-disable-next-line no-path-concat
         // eslint-disable-next-line no-useless-concat
 
-        "dist/models/**/*.entity.js",
+        'dist/models/**/*.entity.js',
         // "./src/models/**/*.ts"
         // User,
         // Appointment,
@@ -30,16 +24,16 @@ module.exports = {
         // Drink,
         // EventType
     ],
-    "migrations": [
-        "./dist/database/**/migrations/*.js",
+    migrations: [
+        './dist/database/**/migrations/*.js',
         // "./src/database/**/migrations/*.ts"
     ],
-    "cli": {
-        "migrationsDir": "./src/database/migrations",
-        "entitiesDir": [
+    cli: {
+        migrationsDir: './src/database/migrations',
+        entitiesDir: [
             // './dist/models',
             // eslint-disable-next-line no-useless-concat
-            "dist/models/"
+            'dist/models/',
         ],
         // "entitiesDir": "./src/models"
     },
